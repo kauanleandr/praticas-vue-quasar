00de539fa6bd2b563f2c9a9a7585167828effb7a
kauan moreira <perry@ricardo.vigliano>
2024-10-16 16:24:45 -0300

    bora praticar

---
+++ my-app/src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="q-pa-md example-row-equal-width">
+    <q-btn push color="teal" label="Change image" @click="refresh" />
+
+    <q-img
+      :src="url"
+      spinner-color="white"
+      style="height: 140px; max-width: 150px"
+    />
+</div>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  name: 'HelloWorld'
+}
+</script>
